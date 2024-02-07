a44f8d89856b2e6fede8684303ecb6ef1fedc27f
Jimy <jimyhoulbrook@gmail.com>
2024-02-07 11:17:21 +0000

    Started making frontend
    Added Send Dir function to router
    Added topnav.css
    Restructure view folder to feature partials and pages
    Created partial topnav.ejs

--- backend/router.js
+++ backend/router.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const fs = require('fs');
+
 class Router{
     // Router for the express application
     constructor(app){
@@ -10,10 +13,29 @@ class Router{
 
     routeGet(){
         this.app.get('/', (req, res) => {
-            res.render('home', {Text: "Hello World"});
+            console.log('user connecting to home')
+            res.render('pages/home', {user: null});
         })
+
+        this.sendDir(path.join(__dirname, '../static'));
     }
     routePost(){}
+
+    // Send all files in a directory
+    // does NOT send subdirectories
+    sendDir(dir){
+        if(!dir)
+        return;
+
+        const files = fs.readdirSync(dir);
+
+        for(const file of files){
+            this.app.get(`/static/${file}`, (req, res) => {
+                res.sendFile(path.join(dir, `${file}`));
+            });
+        }
+        console.log(this.app._router.stack);
+    }
 }
 
 module.exports = Router;
